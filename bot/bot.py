467f54bdd1a349c50f2ca2404e3f6e45edc71259
Nisarg-M-Patel <patel.m.nisarg@gmail.com>
2024-01-01 17:10:10 -0800

    Add logging, order execution
    Added functionality to execute orders and then save the logs to a log
    file and trade logs to another for data analysis

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,7 @@ import json
 import os
 import time
 import datetime
+import sys
 
 client = Client(config("API_KEY"), config("SECRET_KEY"), testnet= True)
 asset = "BTCUSDT"
@@ -39,6 +40,7 @@ def create_account():
      with open("bot_account.json", "w") as F:
          F.write(json.dumps(account)) 
 
+"""method to log messages to a file"""
 def log(msg):
     print(f"LOG: {msg}")
     if not os.path.isdir("logs"):
@@ -50,15 +52,56 @@ def log(msg):
     with open(f"logs/{today}.txt", "a+") as log_file:
         log_file.write(f"{time} : {msg}\n")
     
+def do_trade(account, client, asset, side, quantity):
+    
+    if (side == "buy"):
+        order = client.order_market_buy(
+            symbol = asset,
+            quantity = quantity
+        )
+        account['is_buying'] = False
+    
+    else:
+        order = client.order_market_sell(
+            symbol = asset,
+            quantity = quantity
+        )
+        account['is_buying'] = True
+    order_id = order["orderId"]
+    while order["status"] != "FILLED":
+        order = client.get_order(
+           symbol = asset,
+           orderId = order_id 
+        )
+        time.sleep(1) 
+    price_paid = sum([float(fill["price"]) * float(fill["qty"]) for
+                  fill in order["fills"]])
+    print(order)
+    log_trade(asset, side, price_paid, quantity)
+    with open("bot_account.json", "w") as f:
+        f.write(json.dumps(account))
 
-log(asset)
+def log_trade(sym, side, price, amount):
+    log(f"{side} {amount} {sym} for {price}")
+    
+    if not os.path.isdir("trades"):
+        os.mkdir("trades")
 
+    now = datetime.datetime.now()
+    today = now.strftime("%Y-%m-%d")
+    time = now.strftime("%H-%M-%S")
+
+    if not os.path.isfile(f"trades/{today}.csv"):
+        with open(f"trades/{today}.csv", "w") as trade_file:
+            trade_file.write("sym,side,amount,price\n")
+    with open(f"trades/{today}.csv", "a+") as trade_file:
+            trade_file.write(f"{sym},{side},{amount},{price}\n")
 
 """initial rsi value"""
 rsi = get_rsi("BTCUSDT")
 old_rsi = rsi
-entry = 0
-exit = 0
+entry = 40
+exit = 60
 
 while True:
     try:
@@ -67,16 +110,16 @@ while True:
             create_account()
         with open("bot_account.json") as f:
             account = json.load(f)
+        print(account)
         old_rsi = rsi
         rsi = get_rsi(asset=asset)
         if account["is_buying"]:
             if rsi < entry and old_rsi > entry:
-                #trade
-                pass
+                do_trade(account, client, asset, "buy", "0.01")
             if rsi > exit and old_rsi < exit:
-                #trade
-                pass
+                do_trade(account, client, asset, "sell","0.01")
         print(rsi)
         time.sleep(10)
-    except:
-        pass
+    except Exception as exception:
+        log("ERROR:  " + str(exception))
+        sys.exit()
